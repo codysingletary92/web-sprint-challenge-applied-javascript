eeb041f32f2c894de5f714da1d64194732490c5c
samuel singletary <codysingletary92@gmail.com>
2021-02-21 15:29:19 -0500

    Header Complete

--- components/Header.js
+++ components/Header.js
@@ -11,4 +11,29 @@
 // Use your function to create a header
 // and append it to the DOM inside the div.header-container
 
-function Header() {}
+function Header() {
+    let header = document.createElement('div');
+    let date = document.createElement('span');
+    let h1 = document.createElement('h1');
+    let temp = document.createElement('span');
+
+    header.classList.add('header')
+    date.classList.add('date')
+    temp.classList.add('temp')
+
+    header.appendChild(date);
+    header.appendChild(h1);
+    header.appendChild(temp);
+
+    date.textContent = "MARCH 28, 2020"
+    h1.textContent = "Lambda Times"
+    temp = "98o"
+
+    // ().forEach( item => header.appendChild(item))
+
+    return header
+}
+
+
+headerContainer = document.querySelector('.header-container')
+headerContainer.appendChild(Header())
